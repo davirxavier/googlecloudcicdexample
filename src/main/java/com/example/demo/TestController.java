09db12f0e30fd40c0bddce2e0436206d62bc8fb8
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2023-06-04 16:00:15 -0300

    change path

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/test")
+@RequestMapping(path = "/")
 public class TestController {
     @GetMapping
     public String teste() {
