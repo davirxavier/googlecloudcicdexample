a2619b6261855e282f4e80649cf86c16ad92d54a
DaviXavier <47609623+davirxavier@users.noreply.github.com>
2023-06-05 18:58:51 -0300

    Update TestController.java

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -9,21 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String teste() {
-        return """<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-    <style>
-      body {
-        font-size: 100px;
-      }
-    </style>
-</head>
-<body>
-  <img src="https://pbs.twimg.com/media/EixcAcmWoAIPrMb.jpg">
-</body>
-</html>""";
+        return "TÓPICOS AVANÇADOS";
     }
 
 }
